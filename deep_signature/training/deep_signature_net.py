135c8b141a134c453c84ac6e961e2b18f7087c0d
Roy Velich <xenomarz@users.noreply.github.com>
2020-09-26 04:04:35 +0300

    define network arch

--- deep_signature/training/deep_signature_net.py
+++ deep_signature/training/deep_signature_net.py
@@ -1,16 +1,56 @@
 import torch
+import numpy
 
 
 class DeepSignatureNet(torch.nn.Module):
     def __init__(self, sample_points, padding):
-        super(DynamicNet, self).__init__()
-        self.input_linear = torch.nn.Linear(D_in, H)
-        self.middle_linear = torch.nn.Linear(H, H)
-        self.output_linear = torch.nn.Linear(H, D_out)
+        super(DeepSignatureNet, self).__init__()
+        self._cnn = torch.nn.Sequential(
+            torch.nn.Conv2d(
+                in_channels=1,
+                out_channels=64,
+                kernel_size=(2*padding + 1, 2),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(
+                in_channels=64,
+                out_channels=32,
+                kernel_size=(5, 5),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(
+                in_channels=32,
+                out_channels=16,
+                kernel_size=(3, 3),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(
+                in_channels=16,
+                out_channels=8,
+                kernel_size=(3, 3),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.ReLU(),
+        )
+
+        dim_test = torch.unsqueeze(torch.unsqueeze(torch.rand(sample_points, 2), 0), 0)
+        in_features = numpy.prod(self._cnn(dim_test).shape)
+
+        linear_modules = []
+        for _ in range(4):
+            out_features = int(in_features / 2)
+            linear_modules.append(torch.nn.Linear(in_features=in_features, out_features=out_features))
+            linear_modules.append(torch.nn.ReLU())
+            in_features = out_features
+
+        linear_modules.append(torch.nn.Linear(in_features=in_features, out_features=500))
+
+        self._linear = torch.nn.Sequential(*linear_modules)
 
     def forward(self, x):
-        h_relu = self.input_linear(x).clamp(min=0)
-        for _ in range(random.randint(0, 3)):
-            h_relu = self.middle_linear(h_relu).clamp(min=0)
-        y_pred = self.output_linear(h_relu)
-        return y_pred
+        cnn_output = self._cnn(x)
+        output = self._linear(cnn_output)
+        return output
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 import numpy
 from deep_signature.training.deep_signature_dataset import DeepSignatureDataset
-
+from deep_signature.training.deep_signature_net import DeepSignatureNet
 
 if __name__ == '__main__':
     epochs = 10
@@ -29,6 +29,8 @@ if __name__ == '__main__':
 
     train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler)
     validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=validation_sampler)
+
+    net = DeepSignatureNet(sample_points=500, padding=2).cuda()
 
     for epoch in range(epochs):
         for i, data in enumerate(train_loader, 0):
