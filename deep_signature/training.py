912e176b50239b1195cd244321b93312b329aa07
Roy Velich <xenomarz@users.noreply.github.com>
2020-09-27 00:10:43 +0300

    updates

--- deep_signature/training.py
+++ deep_signature/training.py
@@ -46,39 +46,15 @@ class DeepSignatureNet(torch.nn.Module):
     def __init__(self, sample_points, padding):
         super(DeepSignatureNet, self).__init__()
 
-        self._cnn = torch.nn.Sequential(
-            torch.nn.Conv2d(
-                in_channels=1,
-                out_channels=64,
-                kernel_size=(2*padding + 1, 2),
-                padding=padding,
-                padding_mode='circular'),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(
-                in_channels=64,
-                out_channels=32,
-                kernel_size=(5, 5),
-                padding=padding,
-                padding_mode='circular'),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(
-                in_channels=32,
-                out_channels=16,
-                kernel_size=(3, 3),
-                padding=padding,
-                padding_mode='circular'),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(
-                in_channels=16,
-                out_channels=8,
-                kernel_size=(3, 3),
-                padding=padding,
-                padding_mode='circular'),
-            torch.nn.ReLU()
-        )
+        self._cnn1 = DeepSignatureNet.cnn_block(in_channels=1, padding=padding)
+        self._cnn2 = DeepSignatureNet.cnn_block(in_channels=64, padding=padding)
+        self._cnn3 = DeepSignatureNet.cnn_block(in_channels=64, padding=padding)
 
         dim_test = torch.unsqueeze(torch.unsqueeze(torch.rand(sample_points, 2), 0), 0)
-        in_features = numpy.prod(self._cnn(dim_test).shape)
+        cnn1_out = self._cnn1(dim_test)
+        cnn2_out = self._cnn2(cnn1_out)
+        cnn3_out = self._cnn3(cnn2_out)
+        in_features = numpy.prod(cnn3_out.shape)
 
         linear_modules = []
         for _ in range(4):
@@ -87,14 +63,58 @@ class DeepSignatureNet(torch.nn.Module):
             linear_modules.append(torch.nn.ReLU())
             in_features = out_features
 
-        linear_modules.append(torch.nn.Linear(in_features=in_features, out_features=500))
+        linear_modules.append(torch.nn.Linear(in_features=in_features, out_features=sample_points))
 
         self._linear = torch.nn.Sequential(*linear_modules)
 
     def forward(self, x):
-        cnn_output = self._cnn(x)
+
+        cnn1_out = self._cnn1(x)
+        cnn2_out = self._cnn2(cnn1_out)
+        cnn3_out = self._cnn3(cnn2_out)
+
+        cnn_output = cnn3_out.reshape([x.shape[0], -1])
         output = self._linear(cnn_output)
         return output
+
+    @staticmethod
+    def cnn_block(in_channels, padding):
+        cnn = torch.nn.Sequential(
+            torch.nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=64,
+                kernel_size=(2*padding + 1, 2),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=(3, 3),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=(3, 3),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=(3, 3),
+                padding=padding,
+                padding_mode='circular'),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=5, padding=2)
+        )
+
+        return cnn
 
 
 class ContrastiveLoss(torch.nn.Module):
@@ -105,6 +125,11 @@ class ContrastiveLoss(torch.nn.Module):
     def forward(self, x1, x2, y):
         diff = x1 - x2
         diff_norm = torch.norm(diff)
-        positive_penalty = torch.sum(y * diff_norm)
-        negative_penalty = torch.sum((1 - y) * torch.max(0, self._mu - diff_norm))
+
+        positive_penalties = y * diff_norm
+        negative_penalties = (1 - y) * torch.max(torch.zeros_like(diff_norm), self._mu - diff_norm)
+
+        positive_penalty = torch.sum(positive_penalties)
+        negative_penalty = torch.sum(negative_penalties)
+
         return (positive_penalty + negative_penalty) / x1.shape[0]
--- train.py
+++ train.py
@@ -1,19 +1,18 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 import numpy
 from deep_signature.training import DeepSignatureDataset
 from deep_signature.training import DeepSignatureNet
 from deep_signature.training import ContrastiveLoss
 
+
 if __name__ == '__main__':
-    epochs = 10
-    batch_size = 8
+    epochs = 3
+    batch_size = 32
     validation_split = .2
     shuffle_dataset = True
     random_seed = 42
-    learning_rate = 0.5e-3
+    learning_rate = 5e-5
 
     dataset = DeepSignatureDataset(dir_path='./dataset2')
     dataset_size = len(dataset)
@@ -34,13 +33,14 @@ if __name__ == '__main__':
 
     net = DeepSignatureNet(sample_points=600, padding=2).cuda()
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
-    loss_fn = ContrastiveLoss()
+    loss_fn = ContrastiveLoss(3)
 
+    best_accumulated_avg_loss = 0
     for epoch in range(epochs):
-        print(f' - Epoch #{epoch}:')
+        print(f' - Training Epoch #{epoch}:')
         running_loss = 0
         for batch, data in enumerate(train_loader, 0):
-            print(f'    - Batch #{batch}:')
+            # print(f'    - Batch #{batch}:')
 
             x1 = data['curves'][0]
             x2 = data['curves'][1]
@@ -58,5 +58,37 @@ if __name__ == '__main__':
 
             # print statistics
             running_loss += loss.item()
-            if batch % 10 == 0:    # print every 2000 mini-batches
-                print(f'[Epoch {epoch} | Batch {batch}] - Running Loss = {running_loss}')
+            # if batch % 10 == 0:
+            fill = ' '
+            align = '<'
+            width = 7
+            print(f'    - [Epoch {epoch:{fill}{align}{width}} | Batch {batch:{fill}{align}{width}}] - Running Loss = {running_loss}')
+            running_loss = 0
+
+        print('\n')
+        print(f' - Validating Epoch #{epoch}:')
+        accumulated_avg_loss = 0
+        for batch, data in enumerate(validation_loader, 0):
+            x1 = data['curves'][0]
+            x2 = data['curves'][1]
+            labels = data['labels']
+
+            out1 = net(x1)
+            out2 = net(x2)
+            loss = loss_fn(out1, out2, labels)
+
+            # print statistics
+            accumulated_avg_loss += (loss.item() / batch_size)
+            if batch % 10 == 0:
+                fill = ' '
+                align = '<'
+                width = 7
+                print(f'    - [Batch {batch:{fill}{align}{width}}] - Accumulated Avg. Loss = {accumulated_avg_loss}')
+
+        if epoch == 0:
+            torch.save(net.state_dict(), './models/best_model.pt')
+            best_accumulated_avg_loss = accumulated_avg_loss
+        else:
+            if accumulated_avg_loss < best_accumulated_avg_loss:
+                torch.save(net.state_dict(), './models/best_model.pt')
+                best_accumulated_avg_loss = accumulated_avg_loss
