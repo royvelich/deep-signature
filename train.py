583c5eb6f961cda54f8bfd7d69cf86a354599065
Roy Velich <xenomarz@users.noreply.github.com>
2020-10-29 23:38:13 +0200

    updates

--- train.py
+++ train.py
@@ -5,9 +5,9 @@ from deep_signature.training import ContrastiveLoss
 from deep_signature.training import ModelTrainer
 
 if __name__ == '__main__':
-    epochs = 100
-    batch_size = 128
-    learning_rate = 1e-3
+    epochs = 30
+    batch_size = 64
+    learning_rate = 1e-4
 
     torch.set_default_dtype(torch.float64)
     dataset = DeepSignatureDataset()
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     model = DeepSignatureNet(sample_points=13).cuda()
     print(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    loss_fn = ContrastiveLoss(0.1)
+    loss_fn = ContrastiveLoss(1)
     model_trainer = ModelTrainer(model=model, loss_fn=loss_fn, optimizer=optimizer)
     model_trainer.fit(dataset=dataset, epochs=epochs, batch_size=batch_size, results_base_dir_path='C:/deep-signature-data/results')
