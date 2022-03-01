5d1729a4a46f37eaa49039a34ecdcc691c3caba9
Roy Velich <6681067+royvelich@users.noreply.github.com>
2022-03-02 01:28:41 +0200

    Update train_arclength_affine.bash

--- train_arclength_affine.bash
+++ train_arclength_affine.bash
@@ -10,9 +10,9 @@ export PYTHONPATH=.
 
 /opt/conda/envs/deep-signature/bin/python applets/level_curves/train/train_arclength.py \
 --group "affine" \
---level_curves_euclidean_arclength_tuplets_results_dir_path 'deep-signature/level-curves/results/tuplets/arclength/euclidean' \
---level_curves_similarity_arclength_tuplets_results_dir_path 'deep-signature/level-curves/results/tuplets/arclength/similarity' \
---level_curves_equiaffine_arclength_tuplets_results_dir_path 'deep-signature/level-curves/results/tuplets/arclength/equiaffine' \
---level_curves_affine_arclength_tuplets_results_dir_path 'deep-signature/level-curves/results/tuplets/arclength/affine' \
---level_curves_dir_path_train 'deep-signature/level-curves/curves/train' \
---level_curves_dir_path_validation 'deep-signature/level-curves/curves/validation' \
+--level_curves_euclidean_arclength_tuplets_results_dir_path '/deep-signature-data/level-curves/results/tuplets/arclength/euclidean' \
+--level_curves_similarity_arclength_tuplets_results_dir_path '/deep-signature-data/level-curves/results/tuplets/arclength/similarity' \
+--level_curves_equiaffine_arclength_tuplets_results_dir_path '/deep-signature-data/level-curves/results/tuplets/arclength/equiaffine' \
+--level_curves_affine_arclength_tuplets_results_dir_path '/deep-signature-data/level-curves/results/tuplets/arclength/affine' \
+--level_curves_dir_path_train '/deep-signature-data/level-curves/curves/train' \
+--level_curves_dir_path_validation '/deep-signature-data/level-curves/curves/validation' \
