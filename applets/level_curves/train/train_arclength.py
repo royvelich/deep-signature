93a88b8234dfa5a4ae9b7d020c17392df15c09f2
Roy Velich <6681067+royvelich@users.noreply.github.com>
2022-03-18 01:40:25 +0200

    update

--- applets/level_curves/train/train_arclength.py
+++ applets/level_curves/train/train_arclength.py
@@ -218,7 +218,7 @@ if __name__ == '__main__':
     model = DeepSignatureArcLengthNet(sample_points=args.supporting_points_count)
     model.cuda(device)
 
-    model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    # model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank])
 
     if rank == 0:
--- train_arclength_affine.bash
+++ train_arclength_affine.bash
@@ -6,5 +6,4 @@ export PYTHONPATH=.
 
 /opt/conda/envs/deep-signature/bin/python applets/level_curves/train/train_arclength.py \
 --group "affine" \
---data-dir "/deep-signature-data" \
---dist-backend 'mpi'
+--data-dir "/deep-signature-data"
