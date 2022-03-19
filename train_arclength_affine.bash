f88b25b962d1b0a30d03188cdd5c02759fec5980
Roy Velich <6681067+royvelich@users.noreply.github.com>
2022-03-19 19:17:06 +0200

    Update train_arclength_affine.bash

--- train_arclength_affine.bash
+++ train_arclength_affine.bash
@@ -7,7 +7,7 @@ export PYTHONPATH=.
 /opt/conda/envs/deep-signature/bin/python applets/level_curves/train/train_arclength.py \
 --group "affine" \
 --data-dir "/deep-signature-data" \
---train-batch-size 250000 \
---validation-batch-size 50000 \
+--train-batch-size 256000 \
+--validation-batch-size 100000 \
 --supporting-points-count 5 \
 --learning-rate 0.001
