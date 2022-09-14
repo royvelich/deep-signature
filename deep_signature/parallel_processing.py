cd014199199b4465e222a3a1a20d5d38220f49c5
Roy Velich <6681067+royvelich@users.noreply.github.com>
2022-09-14 04:56:30 +0300

    Update parallel_processing.py

--- deep_signature/parallel_processing.py
+++ deep_signature/parallel_processing.py
@@ -22,7 +22,7 @@ class ParallelProcessor(ABC):
             self._items.extend(curve)
 
         print('    - Creating pool... ', end='')
-        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+        pool = multiprocessing.Pool(processes=50)
         print('Done.')
 
         iterable = list(range(items_count))
