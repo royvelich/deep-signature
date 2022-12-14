5120ea8cfd9f766781fbbb709aebd8dc85a8b2d4
Roy Velich <6681067+royvelich@users.noreply.github.com>
2022-12-14 14:35:34 +0200

    Update generate_curves.bash

--- generate_curves.bash
+++ generate_curves.bash
@@ -9,10 +9,10 @@ python ./applets/generation/generate_planar_curves_from_images.py \
 --images_base_dir_path /data/images/$1 \
 --curves_base_dir_path /data/curves/$1 \
 --min_points_count 300 \
---max_points_count 5000 \
+--max_points_count 6000 \
 --kernel_sizes 3 5 9 17 33 \
 --contour_levels 0.3 0.5 0.7 \
---num_workers 1 \
+--num_workers $2 \
 --min_equiaffine_std 0.05 \
 --smoothing_iterations 6 \
 --smoothing_window_length 99 \
