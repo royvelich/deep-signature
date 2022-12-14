882b8b2a625c2684848cc76dd81141bd2324f7bd
Roy Velich <6681067+royvelich@users.noreply.github.com>
2022-12-14 14:58:49 +0200

    Update utils.py

--- deep_signature/core/utils.py
+++ deep_signature/core/utils.py
@@ -194,6 +194,5 @@ def save_tap(dir_path: Path, typed_argument_parser: Tap, file_name: str = 'args.
 def save_codebase(dir_path: Path):
     repo = git.Repo('.', search_parent_directories=True)
     codebase_source_dir_path = repo.working_tree_dir
-    print(codebase_source_dir_path)
     codebase_destination_dir_path = dir_path / 'code'
-    shutil.copytree(src=codebase_source_dir_path, dst=codebase_destination_dir_path, ignore=shutil.ignore_patterns('.git', '.idea', '__pycache__'))
+    shutil.copytree(src=(codebase_source_dir_path / 'work'), dst=codebase_destination_dir_path, ignore=shutil.ignore_patterns('.git', '.idea', '__pycache__'))
