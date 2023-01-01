5b387d224f9daa5bcd2ba9af960931b4780d3ee4
Roy Velich <6681067+royvelich@users.noreply.github.com>
2023-01-01 21:15:37 +0200

    Update evaluation.py

--- deep_signature/manifolds/planar_curves/evaluation.py
+++ deep_signature/manifolds/planar_curves/evaluation.py
@@ -100,10 +100,12 @@ class PlanarCurvesShapeMatchingEvaluatorTask(ParallelProcessingTask):
     def _process(self, *argv):
         query_curves_file_path = self._benchmark_dir_path / ShapeMatchingBenchmarkCurvesGeneratorTask.get_relative_file_path(curves_file_name=self._curves_file_name, sampling_ratio=self._sampling_ratio, multimodality=self._multimodality, group_name=self._group_name)
         database_curves_file_path = self._benchmark_dir_path / ShapeMatchingBenchmarkCurvesGeneratorTask.get_relative_file_path(curves_file_name=self._curves_file_name, sampling_ratio=1.0, multimodality=self._multimodality, group_name=self._group_name)
-        query_curves = numpy.load(file=str(query_curves_file_path), allow_pickle=True)
-        query_curve = PlanarCurve(points=query_curves[self._query_curve_id])
-        database_curves = numpy.load(file=str(database_curves_file_path), allow_pickle=True)
-        database_curve = PlanarCurve(points=database_curves[self._database_curve_id])
+        with numpy.load(file=str(query_curves_file_path), allow_pickle=True) as query_curves:
+            query_curve = PlanarCurve(points=query_curves[self._query_curve_id])
+
+        with numpy.load(file=str(database_curves_file_path), allow_pickle=True) as database_curves:
+            database_curve = PlanarCurve(points=database_curves[self._database_curve_id])
+
         score = self._planar_curves_signature_comparator.compare_signatures(curve1=query_curve, curve2=database_curve)
         data = {
             'curves_file_name': [self._curves_file_name],
