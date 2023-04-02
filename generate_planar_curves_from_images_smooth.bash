0573a93e9d9085e3e923144c6c9b5a3f9f494e43
Roy Velich <6681067+royvelich@users.noreply.github.com>
2023-04-02 12:24:21 +0300

    Update generate_planar_curves_from_images_smooth.bash

--- generate_planar_curves_from_images_smooth.bash
+++ generate_planar_curves_from_images_smooth.bash
@@ -5,14 +5,14 @@ export PYTHONPATH=.
 
 python ./applets/generation/generate_planar_curves_from_images.py \
 --seed 42 \
---max_tasks $3 \
+--max_tasks $2 \
 --images_base_dir_path /data/images \
 --results_base_dir_path /data/curves/corr_exp \
 --min_points_count 1500 \
 --max_points_count 6000 \
 --kernel_sizes 3 5 9 17 33 \
 --contour_levels 0.1 0.3 0.5 0.7 0.9 \
---num_workers $2 \
+--num_workers $1 \
 --min_equiaffine_std 0.1 \
 --smoothing_iterations 6 \
 --smoothing_window_length 99 \
